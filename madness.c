e4996c2c2c59a135742cdd009e2694b4559c2048
Aritra Sarkar <aritra1911@yahoo.com>
2021-03-01 10:52:08 +0530

    Implement RSA Encryption in multiblock fashion

--- madness.c
+++ madness.c
@@ -4,13 +4,14 @@
 
 #define BUFFER_SIZE 512
 
+int rsa_encrypt(RSA*, FILE*, FILE*);
+
 int main(void) {
     RSA* private_key = NULL;
     RSA* public_key = NULL;
-    FILE* fp;
+    FILE *fp, *fpin, *fpout;
     char buf[BUFFER_SIZE];
     size_t len;
-
 
     // FETCH KEY PAIR ///////////////////////////////////
     fp = fopen("key.pem", "r");
@@ -26,35 +27,13 @@ int main(void) {
     printf("Private key size = %d\n", RSA_bits(private_key));
     printf("Public key size = %d\n", RSA_bits(public_key));
 
-    // Read message to be encrypted
-    fp = fopen("message.txt", "r");
-    len = fread(buf, 1, 1024, fp);
-    fclose(fp);
-    //printf("%s\n", buf);
-    printf("%u bytes read from `message.txt'\n", len);
-
-    // Check if message length is OK
-    // Quoting from man page:
-    // "flen must not be more than RSA_size(rsa) - 42 for RSA_PKCS1_OAEP_PADDING"
-    if (len > RSA_size(public_key) - 42) {
-        fprintf(stderr, "Message length too long!\nMust not be greater than %u\n", RSA_size(public_key) - 42);
-        return 1;
-    }
-
-    // RSA ENCRYPTION ////////////////////////////////////////////////////////////////////////////////////////////////
-    char* encrypted_message = malloc(RSA_size(public_key));
-    if (RSA_public_encrypt(len, buf, encrypted_message, public_key, RSA_PKCS1_OAEP_PADDING) != RSA_size(public_key)) {
-        ERR_print_errors_fp(stderr);
-        return 1;
-    }
-
-    // Write it out to a file
-    fp = fopen("encrypted_message.txt", "w");
-    len = fwrite(encrypted_message, 1, RSA_size(public_key), fp);
-    free(encrypted_message);
-    fclose(fp);
-    printf("%u bytes written to `encrypted_message.txt'\n", len);
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // RSA ENCRYPTION //////////////////////////
+    fpin = fopen("message.txt", "r");
+    fpout = fopen("encrypted_message.txt", "w");
+    rsa_encrypt(public_key, fpin, fpout);
+    fclose(fpin);
+    fclose(fpout);
+    ////////////////////////////////////////////
 
     // Read encrypted file
     fp = fopen("encrypted_message.txt", "r");
@@ -77,4 +56,38 @@ int main(void) {
     ////////////////////////////////////////////////////////////////////////////////////////////
 
     return 0;
+}
+
+int rsa_encrypt(RSA* public_key, FILE* infile, FILE* outfile) {
+    int key_size = RSA_size(public_key);
+    char *buf, *encbuf;
+    size_t len;
+
+    // Quoting from man page:
+    // "flen must not be more than RSA_size(rsa) - 42 for RSA_PKCS1_OAEP_PADDING"
+    buf = malloc(key_size - 42);
+    // Encrypted block with padding will be `key_size' bytes long
+    encbuf = malloc(key_size);
+
+    do {
+        // Read blocks of data from `infile' and encrypt them and write them out to `outfile', one block at a time.
+        // Each block is at most (`key_size' - 42) bytes long. Do this in a loop until eof(infile) is encountered.
+
+        len = fread(buf, 1, key_size - 42, infile);
+        printf("%u bytes read\n", len);
+
+        if ((len = RSA_public_encrypt(len, buf, encbuf, public_key, RSA_PKCS1_OAEP_PADDING)) != key_size) {
+            ERR_print_errors_fp(stderr);
+            free(buf);
+            free(encbuf);
+            return -1;
+        }
+
+        // Write it out to a file
+        len = fwrite(encbuf, 1, len, outfile);
+        printf("%u bytes written\n", len);
+    } while (!feof(infile));
+
+    free(buf);
+    free(encbuf);
 }
